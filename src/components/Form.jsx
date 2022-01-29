0f4efd677f4d437b7cc18615c4980b28d7365f31
Tambouil <60550907+Tambouil@users.noreply.github.com>
2022-01-29 15:36:05 +0100

    project structure

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="form-component">
+      <div className="form-container">
+        <form action=""></form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
