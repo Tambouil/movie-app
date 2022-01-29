859dc817c512df64bce4196a89be12960962b38e
Tambouil <60550907+Tambouil@users.noreply.github.com>
2022-01-29 18:29:06 +0100

    card component data fetch

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card";
 
 const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=star&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, []);
+
   return (
     <div className="form-component">
       <div className="form-container">
-        <form action=""></form>
+        <form>
+          <input
+            type="text"
+            placeholder="Entrez le titre d'un film"
+            id="serach-input"
+          />
+          <input type="submit" value="Rechercher" />
+        </form>
+        <div className="btn-sort-container">
+          <div className="btn-sort" id="goodToBad">
+            Top<span>➜</span>
+          </div>
+          <div className="btn-sort" id="badToGood">
+            Flop<span>➜</span>
+          </div>
+        </div>
+      </div>
+      <div className="result">
+        {moviesData.slice(0, 12).map((movie) => (
+          <Card key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
   );
